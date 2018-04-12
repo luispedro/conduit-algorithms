fe3c9472b3e920b288e0a2a43c1f3779e4abd530
Luis Pedro Coelho <luis@luispedro.org>
2018-04-12 09:20:57 +0200

    BEN Benchmark Async.ByteString module

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,6 +3,8 @@
 import Criterion.Main (defaultMain, nfIO, bench, bgroup, Benchmarkable)
 import Control.Monad.Trans.Resource
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
 import           Control.DeepSeq (NFData)
 import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Binary as CB
@@ -23,6 +25,17 @@ progress n i = C.yield n >> progress (n + 3) (i - 1)
 nfConduit :: NFData a => C.ConduitT () C.Void (ResourceT IO) a -> Benchmarkable
 nfConduit = nfIO . C.runConduitRes
 
+countC = countC' (0 :: Int)
+    where
+        countC' !n = C.await >>= \case
+            Nothing -> return n
+            Just _ -> countC' (n+1)
+sumC = sumC' (0 :: Int)
+    where
+        sumC' !n = C.await >>= \case
+            Nothing -> return n
+            Just v -> sumC' (n+v)
+
 main = defaultMain
     [ bgroup "merge"
         [ bench "mergeC2" $ nfConduit (CAlg.mergeC2 (progress 1 1000) (progress 30 2000) .| CL.fold (+) (0 :: Int))
@@ -32,6 +45,11 @@ main = defaultMain
         [ bench "asyncGzipToFile" $ nfConduit (CB.sourceFile "test_data/input.txt" .| CAlg.asyncGzipToFile "test_data/output.txt.gz")
         , bench "asyncBzip2ToFile" $ nfConduit (CB.sourceFile "test_data/input.txt" .| CAlg.asyncBzip2ToFile "test_data/output.txt.gz")
         , bench "asyncXzToFile" $ nfConduit (CB.sourceFile "test_data/input.txt" .| CAlg.asyncXzToFile "test_data/output.txt.gz")
+        ]
+    , bgroup "async-map"
+        [ bench "filterlines" $ nfConduit (CB.sourceFile "test_data/input.txt" .| CB.lines .| CL.filter (\line -> (read . B8.unpack $ line) > (1000 :: Int)) .| countC)
+        , bench "asyncFilterLinesC" $ nfConduit (CB.sourceFile "test_data/input.txt" .| CAlg.asyncFilterLinesC 8 (\line -> (read . B8.unpack $ line) > (1000 :: Int)) .| countC)
+        , bench "asyncMapLineGroupsC" $ nfConduit (CB.sourceFile "test_data/input.txt" .| CAlg.asyncMapLineGroupsC 8 (length . filter (\line -> (read . B8.unpack $ line) > (1000 :: Int))) .| sumC)
         ]
     ]
 
